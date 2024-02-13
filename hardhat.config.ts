87ec434f7ed00abbcc4f66b67e5944f6e9a8b34d
Henry <11198460+godzillaba@users.noreply.github.com>
2024-02-13 13:59:50 -0500

    sync compiler settings

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,8 +2,31 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "@nomicfoundation/hardhat-foundry";
 
+import { SolidityUserConfig } from "hardhat/types";
+import toml from 'toml';
+import fs from 'fs';
+
 const config: HardhatUserConfig = {
-  solidity: "0.8.19",
+  solidity: getSolidityConfigFromFoundryToml(process.env.FOUNDRY_PROFILE || 'default'),
 };
+
+function getSolidityConfigFromFoundryToml(profile: string): SolidityUserConfig {
+  const data = toml.parse(fs.readFileSync('foundry.toml', 'utf-8'));
+
+  const defaultConfig = data.profile['default'];
+  const profileConfig = data.profile[profile || 'default'];
+
+  const solidity = {
+    version: profileConfig?.solc_version || defaultConfig.solc_version,
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: profileConfig?.optimizer_runs || defaultConfig.optimizer_runs,
+      }
+    }
+  }
+
+  return solidity
+}
 
 export default config;
