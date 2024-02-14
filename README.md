bf93fee415ac04f1b59b4dd8ca149da7d5f5db82
Henry <11198460+godzillaba@users.noreply.github.com>
2024-02-14 12:41:38 -0500

    Formatting (#3)
    * prettier
    
    * eslint running
    
    * package json format and lint commands
    
    * tsconfig options
    
    * update readme
    
    * Formatting ci (#4)
    
    * use node 18

--- README.md
+++ README.md
@@ -5,6 +5,7 @@ A nice template repo that we can build to use for new projects. Maybe eventually
 current setup:
 - hardhat + ethers v6 + foundry
 - solidity compiler config is synced between hardhat and foundry, with foundry.toml being the source
+- use the sdk's formatting options for ts, js, json, md files. use the forge formatter for sol files.
 
 todo:
 - nice libraries that leverage foundry’s fork cheatcodes to mock general cross chain interactions
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,20 +1,22 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
-import "@nomicfoundation/hardhat-foundry";
+import { HardhatUserConfig } from 'hardhat/config'
+import '@nomicfoundation/hardhat-toolbox'
+import '@nomicfoundation/hardhat-foundry'
 
-import { SolidityUserConfig } from "hardhat/types";
-import toml from 'toml';
-import fs from 'fs';
+import { SolidityUserConfig } from 'hardhat/types'
+import toml from 'toml'
+import fs from 'fs'
 
 const config: HardhatUserConfig = {
-  solidity: getSolidityConfigFromFoundryToml(process.env.FOUNDRY_PROFILE || 'default'),
-};
+  solidity: getSolidityConfigFromFoundryToml(
+    process.env.FOUNDRY_PROFILE || 'default'
+  ),
+}
 
 function getSolidityConfigFromFoundryToml(profile: string): SolidityUserConfig {
-  const data = toml.parse(fs.readFileSync('foundry.toml', 'utf-8'));
+  const data = toml.parse(fs.readFileSync('foundry.toml', 'utf-8'))
 
-  const defaultConfig = data.profile['default'];
-  const profileConfig = data.profile[profile || 'default'];
+  const defaultConfig = data.profile['default']
+  const profileConfig = data.profile[profile || 'default']
 
   const solidity = {
     version: profileConfig?.solc_version || defaultConfig.solc_version,
@@ -22,11 +24,11 @@ function getSolidityConfigFromFoundryToml(profile: string): SolidityUserConfig {
       optimizer: {
         enabled: true,
         runs: profileConfig?.optimizer_runs || defaultConfig.optimizer_runs,
-      }
-    }
+      },
+    },
   }
 
   return solidity
 }
 
-export default config;
+export default config
