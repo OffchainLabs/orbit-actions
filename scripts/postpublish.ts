9b4f94a89b8d5e4f0d6883e52a53afde8d4e49cf
Henry <11198460+godzillaba@users.noreply.github.com>
2024-02-22 16:17:10 -0500

    publish.bash

--- scripts/postpublish.ts
+++
@@ -1,5 +0,0 @@
-import * as fs from 'fs'
-fs.rmdirSync('build', { recursive: true })
-fs.renameSync('package.json.bak', 'package.json')
-fs.renameSync('hardhat.config.ts.bak', 'hardhat.config.ts')
-fs.rmSync('hardhat.config.js')
