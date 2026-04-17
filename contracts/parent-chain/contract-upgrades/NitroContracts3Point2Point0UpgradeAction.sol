0f0114328a9d831dbf36b17ff7465edbfb0dcdca
Henry <11198460+godzillaba@users.noreply.github.com>
2026-04-17 09:13:50 -0600

    implement 3.2.0 action

--- contracts/parent-chain/contract-upgrades/NitroContracts3Point2Point0UpgradeAction.sol
+++ contracts/parent-chain/contract-upgrades/NitroContracts3Point2Point0UpgradeAction.sol
@@ -1,12 +1,33 @@
 // SPDX-License-Identifier: Apache-2.0
-pragma solidity 0.8.16;
+pragma solidity ^0.8.16;
 
+interface IRollupAdminLogic {
+    function upgradeSecondaryTo(address newImplementation) external;
+    function upgradeTo(address newImplementation) external;
+}
 /**
  * @title   NitroContracts3Point2Point0UpgradeAction
  * @notice  Upgrade action for nitro contracts v3.2.0
+ * @dev     Only upgrades Rollup contract, other diff between v3.2.0 and v3.1.0 are irrelevant.
  */
 contract NitroContracts3Point2Point0UpgradeAction {
-    function perform() external {
-        // TODO: implement upgrade logic
+    address public immutable newRollupAdminLogicImpl;
+    address public immutable newRollupUserLogicImpl;
+
+    constructor(address _newRollupAdminLogicImpl, address _newRollupUserLogicImpl) {
+        newRollupAdminLogicImpl = _newRollupAdminLogicImpl;
+        newRollupUserLogicImpl = _newRollupUserLogicImpl;
+    }
+
+    function perform(address rollup) external {
+        // skip sequencer inbox upgrade since it only adds custom DA header support
+        // skip OSP upgrade since it only adds custom DA validation support
+
+        // RollupAdminLogic is the primary implementation
+        // RollupUserLogic is the secondary implementation
+        // see: https://github.com/OffchainLabs/nitro-contracts/blob/d9a2f3353ff13c706b7807b097e7ba591d970d85/src/libraries/AdminFallbackProxy.sol#L141-L143
+
+        IRollupAdminLogic(rollup).upgradeTo(newRollupAdminLogicImpl);
+        IRollupAdminLogic(rollup).upgradeSecondaryTo(newRollupUserLogicImpl);
     }
 }
