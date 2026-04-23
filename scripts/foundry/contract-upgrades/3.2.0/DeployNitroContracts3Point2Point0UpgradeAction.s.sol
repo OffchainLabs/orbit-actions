5bbd8d93a6fb53684a5b80764332a8b43171e56e
Henry <11198460+godzillaba@users.noreply.github.com>
2026-04-22 19:24:21 -0600

    fix deployment script for create2

--- scripts/foundry/contract-upgrades/3.2.0/DeployNitroContracts3Point2Point0UpgradeAction.s.sol
+++ scripts/foundry/contract-upgrades/3.2.0/DeployNitroContracts3Point2Point0UpgradeAction.s.sol
@@ -15,14 +15,10 @@ contract DeployNitroContracts3Point2Point0UpgradeActionScript is DeploymentHelpe
     function run() public {
         vm.startBroadcast();
 
-        address newAdminLogic = deployBytecodeWithConstructorFromJSON(
-            "/node_modules/@arbitrum/nitro-contracts-3.2.0/build/contracts/src/rollup/RollupAdminLogic.sol/RollupAdminLogic.json",
-            ""
-        );
-        address newUserLogic = deployBytecodeWithConstructorFromJSON(
-            "/node_modules/@arbitrum/nitro-contracts-3.2.0/build/contracts/src/rollup/RollupUserLogic.sol/RollupUserLogic.json",
-            ""
-        );
+        // see scripts/orbit-versioner/referentContractAddresses.json
+        // there is only one address shared across all chains
+        address newAdminLogic = 0xAb7A44CE7e66963d2116dCe74AB63eeF88266C82;
+        address newUserLogic = 0xedC23dFC7D1e57EC07eA5ff7419634DbAe08Ed2C;
 
         // Deploy the action contract last. The CLI identifies the deployed action
         // by taking the last CREATE from the broadcast file.
